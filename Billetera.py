4bfa3dd395ff36c47e648521ab4ca2250e837536
Elechero <davidhervil@hotmail.com>
2016-05-11 08:53:24 -0430

    Feca y hora actual. Verif. de tipos

--- Billetera.py
+++ Billetera.py
@@ -5,35 +5,46 @@ Created on May 8, 2016
 
 @author: David Hernandez
 '''
-from datetime import date
+import datetime
+import decimal
 class Transaccion:
     
-    def __init__(self, monto, fecha, id):
-        self.monto = monto
-        self.fecha = fecha
+    def __init__(self, monto, id):
+        self.monto = Decimal(monto)
+        self.fecha = datetime.datetime.now()
         self.idEstablecimiento = id
         
 class BilleteraElectronica:
     '''
-    classdocs
+    Classdocs
     '''
     def __init__(self, Id, nombres, apellidos, CI, PIN):
         '''
         Constructor
         '''
-        self.__Id = Id
-        self.nombres_list = nombres.split(' ')
-        self.apellidos_list = apellidos.split(' ')
-        self.CI = CI
-        self.__PIN = PIN
-        self.__recargas = []
-        self.__consumos = []
-        self.__saldo = 0
+        try:
+            self.__Id = Id
+            self.nombres = nombres
+            self.apellidos = apellidos
+            self.CI = CI
+            self.__PIN = PIN
+            self.__recargas = []
+            self.__consumos = []
+            self.__saldo = Decimal(0)
+            assert(type(self.__Id) is int)
+            assert(type(self.nombres_list) is str)
+            assert(type(self.apellidos_list) is str)
+            assert((type(self.CI) is int) and self.CI>0)
+            assert((type(self.__PIN) is str) and self.__PIN.isnumeric())
+        except:
+            raise ValueError('Error de tipo al construir')
     
     def recargar(self, credito):
         self.__recargas.append(credito)
-        if(credito.monto>=0):
+        if(credito.monto>0):
             self.__saldo += credito.monto
+        else:
+            raise ValueError('No se puede recargar montos menores o iguales a 0')
     
     def consumir(self, PIN, debito):
         if(self.__PIN == PIN and debito.monto>=0):
